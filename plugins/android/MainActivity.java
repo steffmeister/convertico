d1f8f667e44808e2f166388e9af92595c7350774
steffmeister <software@steffmeister.at>
2014-01-06 19:32:06 +0100

    Updated templates

--- plugins/android/MainActivity.java
+++ plugins/android/MainActivity.java
@@ -6,6 +6,8 @@ import android.webkit.WebView;
 
 public class MainActivity extends Activity
 {
+    WebView myWebView;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -13,13 +15,18 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        WebView myWebView = (WebView) findViewById(R.id.webView1);
+        myWebView = (WebView) findViewById(R.id.webView1);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setAllowUniversalAccessFromFileURLs(true);
         webSettings.setAllowFileAccessFromFileURLs(true);
         myWebView.loadUrl("file:///android_asset/index.html");
+    }
+    
+    @Override
+    public void onBackPressed() {
+        myWebView.loadUrl("javascript:(function() { var esc = $.Event('keydown', { keyCode: 27 }); $('body').trigger(esc);​ })()");
     }
 }
 
